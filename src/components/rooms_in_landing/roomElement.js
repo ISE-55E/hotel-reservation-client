8295e7f243aeebc6c018548d1cba23cd634da251
Bekzod <toshpulatovbekki7@icloud.com>
2022-12-01 09:23:24 +0500

    sad

--- src/components/rooms_in_landing/roomElement.js
+++ src/components/rooms_in_landing/roomElement.js
@@ -1,11 +1,57 @@
 import {Component} from "react";
 import './roomElement.css';
 import {SwiperSlide} from "swiper/react";
+import {Image} from "react-bootstrap";
+import server from "../../index";
 
 export default class RoomElement extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            category: null,
+            type_room: null,
+        }
+    }
+
+    componentDidMount() {
+        this.getCategoryById(this.props.item.category);
+        this.getTypeRoomById(this.props.item.type_room);
+    }
+
+    getCategoryById = (pk) => {
+        fetch(server + `apps/category/retrieve/${pk}`)
+            .then(res => res.json())
+            .then(data => {
+                this.setState({category: data.name})
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
+
+    getTypeRoomById = (pk) => {
+        fetch(server + `apps/type-room/retrieve/${pk}`)
+            .then(res => res.json())
+            .then(data => {
+                this.setState({type_room: data.name})
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
+
     render() {
         return (
-            <SwiperSlide>Slide 1</SwiperSlide>
+            <SwiperSlide className="justify-content-start align-items-start bg-transparent">
+                <div className="room-image">
+                    <Image src={this.props.item.image1} />
+                </div>
+                <div className="bg-light room-info p-2 room-info-text d-flex flex-column justify-content-start align-items-start">
+                    <h6>{this.props.item.name}</h6>
+                    <p>{this.state.category}, {this.state.type_room}</p>
+                    <p>{this.props.item.floor}-floor, {this.props.item.room_number}-room, {this.props.item.area} kv</p>
+                </div>
+            </SwiperSlide>
         );
     }
 }
--- src/components/rooms_in_landing/roomsInLanding.js
+++ src/components/rooms_in_landing/roomsInLanding.js
@@ -2,9 +2,6 @@ import {Component} from "react";
 import './roomsInLanding.css';
 import { Swiper, SwiperSlide } from "swiper/react";
 import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
 import server from "../../index";
 import {Image} from "react-bootstrap";
 
@@ -13,6 +10,8 @@ export default class RoomsInLanding extends Component {
         super(props);
         this.state = {
             items: [],
+            categoryList: [],
+            typeRoomList: [],
             sort: {
                 category: '',
                 type_room: '',
@@ -35,6 +34,50 @@ export default class RoomsInLanding extends Component {
 
     componentDidMount() {
         this.refreshList();
+        this.refreshCategoryList();
+        this.refreshTypeRoomList();
+    }
+
+    refreshCategoryList = () => {
+        fetch(`${server}apps/category/list/`)
+            .then(res => res.json())
+            .then(data => {
+                this.setState({categoryList: data}, () => {
+                    // console.log(this.state.categoryList)
+                });
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
+
+    refreshTypeRoomList = () => {
+        fetch(`${server}apps/type-room/list/`)
+            .then(res => res.json())
+            .then(data => {
+                this.setState({typeRoomList: data}, () => {
+                    // console.log(this.state.typeRoomList);
+                });
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }
+
+    getCategoryById = (pk) => {
+        for(let i = 0; i < this.state.categoryList.length; ++i) {
+            if (this.state.categoryList[i].id === pk){
+                return this.state.categoryList[i].name;
+            }
+        }
+    }
+
+    getTypeRoomById = (pk) => {
+        for(let i = 0; i < this.state.typeRoomList.length; ++i) {
+            if (this.state.typeRoomList[i].id === pk){
+                return this.state.typeRoomList[i].name;
+            }
+        }
     }
 
     refreshList = () => {
@@ -51,18 +94,25 @@ export default class RoomsInLanding extends Component {
     renderItems = () => {
         const items = this.state.items;
         return items.map((item) => (
-            <SwiperSlide>
-                {/*{item.name}*/}
-                <Image src={item.image1} />
+            <SwiperSlide className="justify-content-start align-items-start bg-transparent">
+                <div className="room-image">
+                    <Image src={item.image1} />
+                </div>
+                <div className="bg-light room-info p-2 room-info-text d-flex flex-column justify-content-start align-items-start">
+                    <h6 className="mx item-name">{item.name}</h6>
+                    <p className="mx">{this.getCategoryById(item.category)}, {this.getTypeRoomById(item.type_room)}</p>
+                    <p className="mx">{item.floor}-floor, {item.room_number}-room, {item.area} kv</p>
+
+                </div>
             </SwiperSlide>
         ))
     }
 
     render() {
         return (
-            <div className="roomsInLanding">
+            <div className="roomsInLanding px-4">
                 <Swiper
-                    spaceBetween={100}
+                    spaceBetween={200}
                     slidesPerView={2}
                     // onSlideChange={() => console.log('slide change')}
                     // onSwiper={(swiper) => console.log(swiper)}
