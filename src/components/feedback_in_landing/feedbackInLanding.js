5abfa12bcbdc170165cec262b0a950ed688622e8
Jobirmirzo <hishimovanvar@gmail.com>
2022-12-02 02:54:14 -0800

    jo1

--- src/components/feedback_in_landing/feedbackInLanding.js
+++ src/components/feedback_in_landing/feedbackInLanding.js
@@ -1,12 +1,29 @@
 import React, {Component} from 'react';
 import "./feedBackInLanding.css";
+import {Button, Col, Form, Row} from "react-bootstrap";
 
 class FeedbackInLanding extends Component {
     render() {
         return (
-            <div className="FeedbackInLanding">
-                
-            </div>
+            <div className="FeedbackInLanding d-flex justify-content-center">
+                <Row className={"w-50"}>
+                    <Col lg={6} md={12} sm={12}>
+                        <img className="ImgFeedBack" src="/images/Vise.jpg" alt=".jpg" />
+                    </Col>
+                    <Col lg={6} md={12} sm={12}>
+                        <Form>
+                            <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                                <Form.Label>Example textarea</Form.Label>
+                                <Form.Control as="textarea" rows={3} />
+                            </Form.Group>
+                            <Button variant="light" type="submit">Clear</Button>{' '}
+                            <Button variant="primary" type="submit">Submit</Button>
+                        </Form>
+                    </Col>
+                </Row>
+             </div>
+
+
         );
     }
 }
