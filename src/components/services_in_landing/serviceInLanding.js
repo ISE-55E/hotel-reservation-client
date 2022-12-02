f2585194b293f88fed73db4a9d4d9a5a5222e325
Bekzod <toshpulatovbekki7@icloud.com>
2022-12-02 12:20:17 +0500

    SD

--- src/components/services_in_landing/serviceInLanding.js
+++ src/components/services_in_landing/serviceInLanding.js
@@ -1,8 +1,6 @@
 import {Component} from "react";
 import './serviceInLanding.css';
 
-import { Navigation, A11y } from 'swiper';
-
 import { Swiper, SwiperSlide } from "swiper/react";
 import 'swiper/css';
 import 'swiper/css/navigation';
@@ -39,42 +37,48 @@ export default class ServiceInLanding extends Component {
             })
     }
 
-    changeActiveItem = (activeIndex) => {
-        // console.log(this.state.activeItem)
+    changeActiveItem = (pk) => {
+        for(let i = 0; i < this.state.items.length; ++i) {
+            if (this.state.items[i].id === pk){
+                this.setState({activeItem: this.state.items[i]});
+                return this.state.items[i];
+            }
+        }
     }
 
     renderItems = () => {
         const items = this.state.items;
         return items.map((item) => (
-            <SwiperSlide key={item.id}>
+            <SwiperSlide key={item.id} accessKey={item.id}>
                 <img src={item.image} alt="Faka" />
             </SwiperSlide>
-        ))
+        ));
     }
 
     render() {
+        let len = this.state.items.length
         return (
             <div className="serviceInLanding">
                 <Swiper
-                    // install Swiper modules
-                    modules={[Navigation, A11y]}
                     spaceBetween={50}
                     slidesPerView={3}
-                    navigation
-                    rewind={true}
-                    centeredSlides={true}
-                    initialSlide={2}
-                    // onSwiper={(swiper) => {
-                    //     this.changeActiveItem(2);
-                    // }}
-                    // onSlideChange={(swiper) => {
-                    //     this.changeActiveItem(swiper.activeIndex);
-                    // }}
+                    centeredSlides
+                    initialSlide={this.state.items.length > 1 ? 1 : 0}
+                    onSlideChange={
+                        (swiper) => {
+                            console.log(swiper.activeIndex);
+                        }
+                    }
+                    onSwiper={
+                        (swiper) => {
+                            console.log(swiper.activeIndex);
+                        }
+                    }
                 >
                     {this.renderItems()}
                 </Swiper>
                 <div className="under-slide pt-5 d-flex flex-column justify-content-center align-items-center">
-                    {/*<h1 className="">{this.state.activeItem.name}</h1>*/}
+                    {/*<h1 className="">{activeItem.name}</h1>*/}
                     {/*<h5 className="">{this.state.activeItem.price} $</h5>*/}
                     {/*<p className="">{this.state.activeItem.info}</p>*/}
                     <Link to="/service">Show More</Link>
