284ca5dbb401c33cedde41fbf75d607bf222381a
Jobirmirzo <hishimovanvar@gmail.com>
2022-12-05 03:31:00 -0800

    jo1

--- src/components/foods_in_landing/foodInLanding.js
+++ src/components/foods_in_landing/foodInLanding.js
@@ -1,11 +1,30 @@
 import {Component} from "react";
 import './foodInLanding.css';
+import {Container, Nav, NavDropdown} from "react-bootstrap";
 
 export default class FoodInLanding extends Component {
     render () {
         return (
             <div className="foodInLanding">
-                <h1>Food</h1>
+                     <div className="FoodPage">
+                        <Nav fill variant="tabs" defaultActiveKey="#">
+                            <Nav.Item>
+                                <Nav.Link className="text-light rounded-0 TabBack" href="#">Salty products</Nav.Link>
+                            </Nav.Item>
+                            <Nav.Item>
+                                <Nav.Link className="text-light rounded-0 TabBack" eventKey="#">Fruits</Nav.Link>
+                            </Nav.Item>
+                            <Nav.Item>
+                                <Nav.Link className="text-light rounded-0 TabBack" eventKey="#">Aquatic Foods</Nav.Link>
+                            </Nav.Item>
+
+                            {/*<Nav.Item>*/}
+                            {/*    <Nav.Link eventKey="disabled" disabled>*/}
+                            {/*        Disabled*/}
+                            {/*    </Nav.Link>*/}
+                            {/*</Nav.Item>*/}
+                        </Nav>
+                    </div>
             </div>
         );
     }
