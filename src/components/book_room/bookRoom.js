961a111968879cd4a8be154c1a5e2eeb8b02bae8
Bekzod <toshpulatovbekki7@icloud.com>
2022-12-06 11:14:50 +0500

    gt

--- src/components/book_room/bookRoom.js
+++ src/components/book_room/bookRoom.js
@@ -1,10 +1,34 @@
 import React, {Component} from 'react';
-import {Carousel, Col, Container, Form, FormControl, FormGroup, Modal, Row} from "react-bootstrap";
+import {Button, Carousel, Col, Container, Form, FormControl, FormGroup, FormLabel, Modal, Row} from "react-bootstrap";
 import './bookRoom.css';
 
 class BookRoom extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            bookingDetails: {
+                room: this.props.item.id,
+                price: this.props.item.price,
+                from_time: 0,
+                to_time: 0
+            },
+        }
+    }
+
+    submitBooking = () => {
+    //    post to backend
+    }
+
+    handleChange = (e) => {
+        let {name, value} = e.target;
+
+        const bookingDetails = {...this.state.bookingDetails, [name]: value};
+
+        this.setState({bookingDetails}, () => {
+            let from = new Date(this.state.bookingDetails.from_time);
+            let to = new Date(this.state.bookingDetails.to_time);
+            console.log(to.getDate() - from.getDate());
+        });
     }
 
     render() {
@@ -53,49 +77,68 @@ class BookRoom extends Component {
                                 </Carousel.Item>
                             </Carousel>
                         </Col>
-                        <Col xs={7}>
+                        <Col xs={7} className="p-2">
                             <h4>{this.props.item.name}</h4>
                             <p>{this.props.item.floor}-floor, {this.props.item.room_number}-room</p>
                             <p>Single Beds: {this.props.item.single_beds}, Double Beds: {this.props.item.double_beds}</p>
-                            <h5>{this.props.item.price}$</h5>
+                            <h5>Price For One Day: {this.props.item.price}$</h5>
+                            <h5>Total Price: {this.state.bookingDetails.price}$</h5>
                         </Col>
                     </Row>
                     <Row>
                         <Form>
                             <Col className="p-5">
-                                <Container className="p-3 border border-dark rounded">
+                                <Container className="p-3 border rounded shadow">
                                     <Row>
-                                        <Col>
+                                        <Col xs={6}>
                                             <FormGroup>
+                                                <FormLabel>
+                                                    From:
+                                                </FormLabel>
                                                 <FormControl
                                                     type="datetime-local"
+                                                    name="from_time"
+                                                    className="shadow"
+                                                    onChange={this.handleChange}
                                                 />
                                             </FormGroup>
                                         </Col>
-                                        <Col>
+                                        <Col xs={6}>
                                             <FormGroup>
+                                                <FormLabel>
+                                                    To:
+                                                </FormLabel>
                                                 <FormControl
-                                                    type="text"
+                                                    type="datetime-local"
+                                                    name="to_time"
+                                                    className="shadow"
+                                                    onChange={this.handleChange}
                                                 />
                                             </FormGroup>
                                         </Col>
-                                        <Col>
+                                        <Col xs={12}>
                                             <FormGroup>
+                                                <FormLabel>
+                                                    Detail:
+                                                </FormLabel>
                                                 <FormControl
-                                                    type="text"
-                                                />
-                                            </FormGroup>
-                                        </Col>
-                                        <Col>
-                                            <FormGroup>
-                                                <FormControl
-                                                    type="text"
+                                                    as="textarea"
+                                                    name="detail"
+                                                    className="shadow"
+                                                    onChange={this.handleChange}
                                                 />
                                             </FormGroup>
                                         </Col>
                                     </Row>
                                 </Container>
                             </Col>
+                            <Container className="px-5 py-3">
+                                <Row className="px-3">
+                                    <Button type="submit" className="book-button shadow" onClick={this.submitBooking}>
+                                        Book
+                                    </Button>
+                                </Row>
+                            </Container>
                         </Form>
                     </Row>
                 </Container>
