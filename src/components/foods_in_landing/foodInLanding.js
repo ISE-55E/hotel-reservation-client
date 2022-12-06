357555bad8bdf2228d719c013ca8bdf31ac3be9d
Jobirmirzo <hishimovanvar@gmail.com>
2022-12-05 22:22:13 -0800

    ok

---
+++ src/components/foods_in_landing/foodInLanding.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class FoodInLanding extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default FoodInLanding;
