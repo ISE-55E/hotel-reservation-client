235490abb658abbeda8d561de27915c3043cd976
Bekzod <toshpulatovbekki7@icloud.com>
2022-12-18 16:11:27 +0500

    final

--- src/components/feedback_in_landing/feedbackInLanding.js
+++ src/components/feedback_in_landing/feedbackInLanding.js
@@ -1,16 +1,44 @@
 import React, {Component} from 'react';
 import "./feedBackInLanding.css";
-import {Button, Col, Container, FloatingLabel, Form, Row} from "react-bootstrap";
-import * as PropTypes from "prop-types";
-
-function FeedBackDiv(props) {
-    return null;
-}
+import {Button, Col, FloatingLabel, Form, Row} from "react-bootstrap";
+import axios from "axios";
+import server from "../../index";
 
 
 class FeedbackInLanding extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            msg: null
+        }
+    }
+
+    handleChange = (e) => {
+        let { value } = e.target;
+
+        this.setState({msg: value});
+    }
+
+    handleSubmit = () => {
+        const item = {
+            message: this.state.msg
+        }
+
+        axios
+            .post(`${server}apps/feedback/create/`,
+                item,
+            )
+            .then(res => {
+                alert("Feedback sent!!!");
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
+
     render() {
         return (
+            <form className="w-100 d-flex justify-content-center">
                 <Row className="FeedbackPart">
                     <Col className="left d-flex justify-content-end" lg={6} sm={12}>
                         <img src="/images/Vise.jpg" alt=".jpg"/>
@@ -21,14 +49,16 @@ class FeedbackInLanding extends Component {
                                 as="textarea"
                                 placeholder="Leave a comment here"
                                 style={{ height: '100px' }}
+                                onChange={this.handleChange}
                             />
                         </FloatingLabel>
                         <div className="buttons">
-                            <Button className="btn1 rounded-0 m-2 ">Submit</Button>
-                            <Button className="btn2 rounded-0 m-2">Secondary</Button>
+                            <Button className="btn1 rounded-0 m-2" onClick={this.handleSubmit}>Submit</Button>
+                            <Button className="btn2 rounded-0 m-2" type="reset">Secondary</Button>
                         </div>
                     </Col>
                 </Row>
+            </form>
         );
     }
 }
